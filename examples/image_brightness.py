3bd05550b4a3451614dcaf5177499bd2fa6a739b
nawatend <32384185+nawatend@users.noreply.github.com>
2019-04-09 23:13:01 +0200

    code files effects added

---
+++ examples/image_brightness.py
@@ -0,0 +1,12 @@
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+//between 1 to 100
+camera.brightness = 70
+camera.start_recording()
+//record 10 seconds
+sleep(10)
+camera.stop_recording()
+print(‘End Preview’)
